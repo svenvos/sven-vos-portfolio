e08af98581169df89db9c36cc0f328cbd23e8a32
Sven Vos <161502162+svenvos@users.noreply.github.com>
2025-12-31 09:50:02 +0100

    Rendered the Projects section component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Projects from "./components/home/Projects";
+
 export default function Homepage() {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <main className="max-w-7xl mx-auto px-4">
+            <h1 className="leading-tight mt-12 text-center font-black text-[52px]">Fullstack developer</h1>
+            <Projects />
+        </main>
     );
 }
